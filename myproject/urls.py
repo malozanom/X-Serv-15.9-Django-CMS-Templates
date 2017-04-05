1aff9b698a1112971eab018981420b15c27ae078
Your Name <you@example.com>
2017-04-05 19:15:55 +0200

    ejercicio resuelto

---
+++ myproject/urls.py
@@ -0,0 +1,33 @@
+"""myproject URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.8/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import include, url
+from django.contrib import admin
+from cms_templates import views
+from django.contrib.auth.views import login, logout
+
+urlpatterns = [
+    url(r'^annotated/cms_templates/?$', views.show_template,
+        name='Show the database content with a template'),
+    url(r'^annotated/cms_templates/(\d+)$', views.entry_template,
+        name='Show the selected page with a template'),
+    url(r'^cms_templates/?$', views.show, name='Show the database content'),
+    url(r'^cms_templates/(\d+)$', views.entry, name='Show the selected page'),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^logout/?$', logout, {'next_page': '/cms_templates'}),
+    url(r'^login/?$', login),
+    url(r'^annotated/.*', views.error_template,
+        name='Not found with a template'),
+    url(r'.*', views.error, name='Not found'),
+]
